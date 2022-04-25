838889f3cdf274e459fe49867bb2a07c4421dc0d
Jean Bisson <jean.bisson@hotmail.ca>
2022-04-24 23:01:58 -0400

    1 TH with 2 rows: column name, input (filter)

---
+++ Client/Shared/Column.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncidentManager.Client.Shared
+{
+    public partial class Column<TItem>
+    {
+        [Parameter]
+        public string ColumnTitle { get; set; }
+
+        [Parameter]
+        public bool filterable { get; set; }
+
+        [Parameter]
+        public EventCallback<ChangeEventArgs> OnSearchTextChanged { get; set; }
+
+
+        //trying stuff like Radzen does with GridColumn
+
+        [CascadingParameter]
+        public TableComponent<TItem> myTable { get; set; }
+
+        [Parameter]
+        public RenderFragment Columns { get; set; }
+
+        [CascadingParameter]
+        public Column<TItem> Parent { get; set; }
+    }
+}
---
+++ Shared/ColumnDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IncidentManager.Shared
+{
+    public class ColumnDetails
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public bool Filterable { get; set; }
+        public bool Sortable { get; set; }
+    }
+}
